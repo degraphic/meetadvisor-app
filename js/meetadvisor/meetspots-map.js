9b180fbd019534f04f809cb1db4fea958eeff24b
antoineangot <antoine.angot@gmail.com>
2012-04-19 19:15:01 +0200

    infobox sur map
    Signed-off-by: antoineangot <antoine.angot@gmail.com>

--- js/meetadvisor/meetspots-map.js
+++ js/meetadvisor/meetspots-map.js
@@ -106,7 +106,7 @@ MeetspotsMap.prototype = {
 		this.gMapSetMarker(geolocatedLat, geolocatedLng);
 		
 		// some test
-		this.Antoinetestdestrucs();
+		this.showInfoWindow(48.850033, 2.297383, '<br /><a href="#womanmapguest/create-account">inscription a meetadvisor</a><br/>');
     },
 	
     gMapSetPosition: function (lat, lng) {
@@ -126,56 +126,15 @@ MeetspotsMap.prototype = {
 
 	},
 	
-	// ANTOINEEEEEEEEEEEEEEEee
-	// ANTOINEEEEEEEEEEEEEEEee
-	// ANTOINEEEEEEEEEEEEEEEee
-	// ANTOINEEEEEEEEEEEEEEEee
-	// ANTOINEEEEEEEEEEEEEEEee
-	// ANTOINEEEEEEEEEEEEEEEee
-	
-	Antoinetestdestrucs: function () {
-		this.chicago = new google.maps.LatLng(48.850033,2.297383);
-	
+	showInfoWindow: function (lat, lng, message) {
+		var point = new google.maps.LatLng(lat, lng);
 		var coordInfoWindow = new google.maps.InfoWindow();
-        coordInfoWindow.setContent(this.createInfoWindowContent());
-        coordInfoWindow.setPosition(this.chicago);
+		
+        coordInfoWindow.setContent(message);
+        coordInfoWindow.setPosition(point);
         coordInfoWindow.open(this.map);
 	},
-	
-		
-	
-	createInfoWindowContent: function () {
-        var numTiles = 1 << this.map.getZoom();
-        var projection = new MercatorProjection();
-        var worldCoordinate = projection.fromLatLngToPoint(this.chicago);
-        var pixelCoordinate = new google.maps.Point(
-            worldCoordinate.x * numTiles,
-            worldCoordinate.y * numTiles);
-        var tileCoordinate = new google.maps.Point(
-            Math.floor(pixelCoordinate.x / TILE_SIZE),
-            Math.floor(pixelCoordinate.y / TILE_SIZE));
 
-        return ['Chicago, IL',
-                'LatLng: ' + this.chicago.lat() + ' , ' + this.chicago.lng(),
-                'World Coordinate: ' + worldCoordinate.x + ' , ' +
-                  worldCoordinate.y,
-                'Pixel Coordinate: ' + Math.floor(pixelCoordinate.x) + ' , ' +
-                  Math.floor(pixelCoordinate.y),
-                'Tile Coordinate: ' + tileCoordinate.x + ' , ' +
-                  tileCoordinate.y + ' at Zoom Level: ' + this.map.getZoom() +
-				  '<br /><a href="#womanmapguest/create-account">inscription a meetadvisor</a>'
-               ].join('<br>');
-    },
-	
-	
-	// ANTOINEEEEEEEEEEEEEEEee
-	// ANTOINEEEEEEEEEEEEEEEee
-	// ANTOINEEEEEEEEEEEEEEEee
-	// ANTOINEEEEEEEEEEEEEEEee
-	// ANTOINEEEEEEEEEEEEEEEee
-	// ANTOINEEEEEEEEEEEEEEEee
-
-	
 	/* GETTERS - SETTERS */
 	getMap: function() {
 		return this.map;
@@ -187,58 +146,5 @@ MeetspotsMap.prototype = {
 }
 
 
-// ANTOINEEEEEEEEEEEEEEEee
-// ANTOINEEEEEEEEEEEEEEEee
-// ANTOINEEEEEEEEEEEEEEEee
-// ANTOINEEEEEEEEEEEEEEEee
-// ANTOINEEEEEEEEEEEEEEEee
-// ANTOINEEEEEEEEEEEEEEEee
 
-
-var TILE_SIZE = 256;
-
-function MercatorProjection() {
-	this.pixelOrigin_ = new google.maps.Point(TILE_SIZE / 2,
-		TILE_SIZE / 2);
-	this.pixelsPerLonDegree_ = TILE_SIZE / 360;
-	this.pixelsPerLonRadian_ = TILE_SIZE / (2 * Math.PI);
-}
-	  
-	  
-MercatorProjection.prototype.fromLatLngToPoint = function(latLng, opt_point) {
-        var me = this;
-        var point = opt_point || new google.maps.Point(0, 0);
-        var origin = me.pixelOrigin_;
-
-        point.x = origin.x + latLng.lng() * me.pixelsPerLonDegree_;
-
-        // NOTE(appleton): Truncating to 0.9999 effectively limits latitude to
-        // 89.189.  This is about a third of a tile past the edge of the world
-        // tile.
-        var siny = bound(Math.sin(degreesToRadians(latLng.lat())), -0.9999,
-            0.9999);
-        point.y = origin.y + 0.5 * Math.log((1 + siny) / (1 - siny)) *
-            -me.pixelsPerLonRadian_;
-        return point;
-};
-
-MercatorProjection.prototype.fromPointToLatLng = function(point) {
-        var me = this;
-        var origin = me.pixelOrigin_;
-        var lng = (point.x - origin.x) / me.pixelsPerLonDegree_;
-        var latRadians = (point.y - origin.y) / -me.pixelsPerLonRadian_;
-        var lat = radiansToDegrees(2 * Math.atan(Math.exp(latRadians)) -
-            Math.PI / 2);
-        return new google.maps.LatLng(lat, lng);
-};
-
-function bound(value, opt_min, opt_max) {
-	if (opt_min != null) value = Math.max(value, opt_min);
-	if (opt_max != null) value = Math.min(value, opt_max);
-	return value;
-}
-
-function degreesToRadians(deg) {
-    return deg * (Math.PI / 180);
-}
 
