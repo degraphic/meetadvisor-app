1bb22b7fd5a9322fc6d35fc5392013cdf139d8fb
Valerian Gaudeau <valerian.gaudeau@gmail.com>
2012-04-20 00:56:27 +0200

    bugfix navigation login

--- js/meetadvisor/main.js
+++ js/meetadvisor/main.js
@@ -59,7 +59,7 @@ MeetAdvisor.prototype = {
             return false;
 
 		// do i have a session ?
-		if (localStorage.getItem("key") == null) {
+		if (this.current_page != 'login' && localStorage.getItem("key") == null) {
 			// no => go to login
 			if (this.current_page != "createAccount" &&
                 this.current_page != "gender") {
