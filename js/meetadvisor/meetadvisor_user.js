b39ab3eea04db4aabd008186e26564370d50fee6
antoineangot <antoine.angot@gmail.com>
2012-04-19 20:56:15 +0200

    class user
    Signed-off-by: antoineangot <antoine.angot@gmail.com>

--- js/meetadvisor/meetadvisor_user.js
+++ js/meetadvisor/meetadvisor_user.js
@@ -3,20 +3,5 @@ var User = function User() {};
 User.prototype = {
     isFemale: null,
 	email : null,
-	isVIP: null,
-	
-    
-    init: function() {
-	    this.template = {
-		    file: null, 
-		    src: null
-	    };
-	    this.page = {
-		    file: null, 
-		    src: null
-	    };
-        this.partial_files = {};
-        this.partial_srcs = {};
-        this.data = {};
-    },
+	isVIP: null
 }
