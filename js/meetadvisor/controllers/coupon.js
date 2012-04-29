26dd9fd78d0679b19b127247c4c0e160c6f2a514
antoineangot <antoine.angot@gmail.com>
2012-04-29 22:38:19 +0200

    fix coupon
    Signed-off-by: antoineangot <antoine.angot@gmail.com>

--- js/meetadvisor/controllers/coupon.js
+++ js/meetadvisor/controllers/coupon.js
@@ -6,21 +6,9 @@ Coupon.prototype = {
 		
 	},
 	
-	getInfo: function(render_data) {
-		var that = this;
-		
-		var tab = {
-			id: render_data.request_params["id"],
-			};
-		
-		var u = new User();
-		
-		meetadvisor.api.validateCoupon(render_data.request_params["id"], u.getUid());
-		
-		return (tab);
-	},
-	
 	render: function(render_data) {
+		var u = new User();
+		meetadvisor.api.validateCoupon(render_data.request_params["id"], u.getUid());
 		if (meetadvisor.current_venue == null) {
 			this.complicated(render_data);
 		} else { 
@@ -31,7 +19,7 @@ Coupon.prototype = {
 	// j'ai de la data
 	simple: function (render_data) {
 		var id = 1000000000 * Math.random();
-		meetadvisor.current_venue.coupon_id = id.toPrecision(9);
+		meetadvisor.current_venue.coupon_id = id.toPrecision(8);
 		render_data.data = meetadvisor.current_venue;
 		meetadvisor.render(render_data);
 	},
@@ -44,7 +32,7 @@ Coupon.prototype = {
 	
 	wsDataReponse: function (data) {
 		var id = 1000000000 * Math.random();
-		data.coupon_id = id.toPrecision(9);
+		data.coupon_id = id.toPrecision(8);
 		meetadvisor.current_venue.data = data;
 		meetadvisor.render(meetadvisor.current_venue);
 	},
